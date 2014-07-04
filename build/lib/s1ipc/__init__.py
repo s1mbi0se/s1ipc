9dd50756594277571636ea3815e998094ff89542
Matheus Salvia <matheus2740@gmail.com>
2014-07-04 19:06:43 -0300

    initial commit

--- build/lib/s1ipc/__init__.py
+++
@@ -1,4 +0,0 @@
-__author__ = 'matheus2740'
-
-from server import BaseIPCServer, IPCAvailable
-from client import BaseIPCClient
--- build/lib/s1ipc/sharedcache/__init__.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'salvia'
-
-from shared_cache import SharedCache
-from server import SharedCacheServer
-from memoize import Memoize, NonNoneMemoize, ValidativeMemoize, LocalMemoize, LocalNonNoneMemoize, LocalValidativeMemoize
